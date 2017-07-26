5569ab21baa0612d60333f8cfeb154e179587f0b
Brian Batchelor <batchbk@gmail.com>
2017-07-26 13:12:12 -0400

    Completed percolationStats

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -7,32 +7,31 @@ import java.util.Scanner;
 public class Percolation {
     private int[] openSite;
     private int totalOpenSites;
-    private int sideLength;
-    private int srcSite;
-    private int sinkSite;
-    private WeightedQuickUnionUF uf;
-    private int N;
+    private final int sideLength;
+    private final int srcSite;
+    private final int sinkSite;
+    private final WeightedQuickUnionUF uf;
 
-    public Percolation(int n) throws IllegalArgumentException {
+    public Percolation(int n) {
         if(n <= 0) throw new IllegalArgumentException("n is less than 0");
 
         sideLength = n;
-        N = n * n + 2;
-        openSite = new int[N];
+        int num = n * n + 2;
+        openSite = new int[num];
         srcSite = 0;
         openSite[srcSite] = 1;
         sinkSite = 1;
         openSite[sinkSite] = 1;
-        uf = new WeightedQuickUnionUF(N);
+        uf = new WeightedQuickUnionUF(num);
     }
 
     public void open(int row, int col) {
-        if(row > sideLength || col > sideLength){
-            throw new IllegalArgumentException("either row or col exceed limit: " + row +", " + col);
+        if(row > sideLength || col > sideLength) {
+            throw new IllegalArgumentException("either row or col exceed limit: " + row + ", " + col);
         }
         row--;
         col--;
-        if (openSite[matrixToArray(row,col)] == 0) {
+        if(openSite[matrixToArray(row, col)] == 0) {
             int idx = matrixToArray(row, col);
             openSite[idx] = 1;
             connectOpenNeighbors(row, col);
@@ -43,15 +42,15 @@ public class Percolation {
     public boolean isOpen(int row, int col) {
         row--;
         col--;
-        if(row > sideLength || col > sideLength){
+        if(row > sideLength || col > sideLength) {
             throw new IllegalArgumentException("either row or coll exceed limit");
         }
         int idx = matrixToArray(row, col);
         boolean isOpen = false;
-        try {
+        try{
             isOpen = openSite[idx] == 1;
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("row: " + row +", " + "col: " + col + " index: " + idx);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("row: " + row + ", " + "col: " + col + " index: " + idx);
         }
         return isOpen;
     }
@@ -59,10 +58,10 @@ public class Percolation {
     public boolean isFull(int row, int col) {
         row--;
         col--;
-        if(row > sideLength || col > sideLength){
+        if(row > sideLength || col > sideLength) {
             throw new IllegalArgumentException("either row or coll exceed limit");
         }
-        if (isOpen(row, col)) {
+        if(isOpen(row, col)) {
             int idx = matrixToArray(row, col);
             return uf.connected(srcSite, idx);
         }
@@ -74,7 +73,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return uf.connected(srcSite,sinkSite);
+        return uf.connected(srcSite, sinkSite);
     }
 
     private int matrixToArray(int row, int col) {
@@ -83,126 +82,126 @@ public class Percolation {
 
     private void connectOpenNeighbors(int row, int col) {
         int current = matrixToArray(row, col);
-        if (isLeft(col) && isTop(row)) { // Top-left
-            if(openSite[matrixToArray(row, toRight(col))] == 1){
-                uf.union(current,matrixToArray(row, toRight(col)));
+        if(isLeft(col) && isTop(row)) { //Top-left
+            if(openSite[matrixToArray(row, toRight(col))] == 1) {
+                uf.union(current, matrixToArray(row, toRight(col)));
             }
-            if(openSite[matrixToArray(toBottom(row), col)] ==1){
-                uf.union(current,matrixToArray(toBottom(row), col));
+            if(openSite[matrixToArray(toBottom(row), col)] == 1) {
+                uf.union(current, matrixToArray(toBottom(row), col));
             }
-            uf.union(current,srcSite);
-        } else if (isRight(col) && isTop(row)) { //Top- right
-            if(openSite[matrixToArray(row, toLeft(col))] ==1){
-                uf.union(current,matrixToArray(row, toLeft(col)));
+            uf.union(current, srcSite);
+        } else if(isRight(col) && isTop(row)) { //Top- right
+            if(openSite[matrixToArray(row, toLeft(col))] == 1) {
+                uf.union(current, matrixToArray(row, toLeft(col)));
             }
-            if(openSite[matrixToArray(toBottom(row), col)] ==1){
-                uf.union(current,matrixToArray(toBottom(row), col));
+            if(openSite[matrixToArray(toBottom(row), col)] == 1) {
+                uf.union(current, matrixToArray(toBottom(row), col));
             }
-            uf.union(current,srcSite);
-        } else if (isLeft(col) && isBottom(row)) { //Bottom - left
-            if(openSite[matrixToArray(toTop(row), col)] ==1){
-                uf.union(current,matrixToArray(toTop(row), col));
+            uf.union(current, srcSite);
+        } else if(isLeft(col) && isBottom(row)) { //Bottom - left
+            if(openSite[matrixToArray(toTop(row), col)] == 1) {
+                uf.union(current, matrixToArray(toTop(row), col));
             }
-            if(openSite[matrixToArray(row, toRight(col))] ==1){
-                uf.union(current,matrixToArray(row, toRight(col)));
+            if(openSite[matrixToArray(row, toRight(col))] == 1) {
+                uf.union(current, matrixToArray(row, toRight(col)));
             }
-            uf.union(current,sinkSite);
-        } else if (isRight(col) && isBottom(row)) { //Bottom - right
-            if(openSite[matrixToArray(toTop(row), col)]==1){
-                uf.union(current,matrixToArray(toTop(row), col));
+            uf.union(current, sinkSite);
+        } else if(isRight(col) && isBottom(row)) { //Bottom - right
+            if(openSite[matrixToArray(toTop(row), col)] == 1) {
+                uf.union(current, matrixToArray(toTop(row), col));
             }
-            if(openSite[matrixToArray(row, toLeft(col))]==1){
-                uf.union(current,matrixToArray(row, toLeft(col)));
+            if(openSite[matrixToArray(row, toLeft(col))] == 1) {
+                uf.union(current, matrixToArray(row, toLeft(col)));
             }
-            uf.union(current,sinkSite);
-        } else if (isLeft(col)) {
-            if(openSite[matrixToArray(toTop(row), col)] ==1){
-                uf.union(current,matrixToArray(toTop(row), col));
+            uf.union(current, sinkSite);
+        } else if(isLeft(col)) {
+            if(openSite[matrixToArray(toTop(row), col)] == 1) {
+                uf.union(current, matrixToArray(toTop(row), col));
             }
-            if(openSite[matrixToArray(toBottom(row), col)] ==1){
-                uf.union(current,matrixToArray(toBottom(row), col));
+            if(openSite[matrixToArray(toBottom(row), col)] == 1) {
+                uf.union(current, matrixToArray(toBottom(row), col));
             }
-            if(openSite[matrixToArray(row, toRight(col))] ==1){
-                uf.union(current,matrixToArray(row, toRight(col)));
+            if(openSite[matrixToArray(row, toRight(col))] == 1) {
+                uf.union(current, matrixToArray(row, toRight(col)));
             }
-        } else if (isRight(col)) {
-            if(openSite[matrixToArray(toTop(row), col)] ==1){
-                uf.union(current,matrixToArray(toTop(row), col));
+        } else if(isRight(col)) {
+            if(openSite[matrixToArray(toTop(row), col)] == 1) {
+                uf.union(current, matrixToArray(toTop(row), col));
             }
-            if(openSite[matrixToArray(toBottom(row), col)] ==1){
-                uf.union(current,matrixToArray(toBottom(row), col));
+            if(openSite[matrixToArray(toBottom(row), col)] == 1) {
+                uf.union(current, matrixToArray(toBottom(row), col));
             }
-            if(openSite[matrixToArray(row, toLeft(col))] ==1){
-                uf.union(current,matrixToArray(row, toLeft(col)));
+            if(openSite[matrixToArray(row, toLeft(col))] == 1) {
+                uf.union(current, matrixToArray(row, toLeft(col)));
             }
-        } else if (isTop(row)) {
-            if(openSite[matrixToArray(row, toRight(col))] ==1){
-                uf.union(current,matrixToArray(row, toRight(col)));
+        } else if(isTop(row)) {
+            if(openSite[matrixToArray(row, toRight(col))] == 1) {
+                uf.union(current, matrixToArray(row, toRight(col)));
             }
-            if(openSite[matrixToArray(row, toLeft(col))] ==1){
-                uf.union(current,matrixToArray(row, toLeft(col)));
+            if(openSite[matrixToArray(row, toLeft(col))] == 1) {
+                uf.union(current, matrixToArray(row, toLeft(col)));
             }
-            if(openSite[matrixToArray(toBottom(row), col)] ==1){
-                uf.union(current,matrixToArray(toBottom(row), col));
+            if(openSite[matrixToArray(toBottom(row), col)] == 1) {
+                uf.union(current, matrixToArray(toBottom(row), col));
             }
-            uf.union(current,srcSite);
-        } else if (isBottom(row)) {
-            if(openSite[matrixToArray(toTop(row), col)]==1){
-                uf.union(current,matrixToArray(toTop(row), col));
+            uf.union(current, srcSite);
+        } else if(isBottom(row)) {
+            if(openSite[matrixToArray(toTop(row), col)] == 1) {
+                uf.union(current, matrixToArray(toTop(row), col));
             }
-            if(openSite[matrixToArray(row, toRight(col))] ==1){
-                uf.union(current,matrixToArray(row, toRight(col)));
+            if(openSite[matrixToArray(row, toRight(col))] == 1) {
+                uf.union(current, matrixToArray(row, toRight(col)));
             }
-            if(openSite[matrixToArray(row, toLeft(col))] ==1){
-                uf.union(current,matrixToArray(row, toLeft(col)));
+            if(openSite[matrixToArray(row, toLeft(col))] == 1) {
+                uf.union(current, matrixToArray(row, toLeft(col)));
             }
-            uf.union(current,sinkSite);
-        }else{
-            if(openSite[matrixToArray(toBottom(row), col)]==1){
-                uf.union(current,matrixToArray(toBottom(row), col));
+            uf.union(current, sinkSite);
+        } else {
+            if(openSite[matrixToArray(toBottom(row), col)] == 1) {
+                uf.union(current, matrixToArray(toBottom(row), col));
             }
-            if(openSite[matrixToArray(toTop(row), col)]==1){
-                uf.union(current,matrixToArray(toTop(row), col));
+            if(openSite[matrixToArray(toTop(row), col)] == 1) {
+                uf.union(current, matrixToArray(toTop(row), col));
             }
-            if(openSite[matrixToArray(row, toRight(col))]==1){
-                uf.union(current,matrixToArray(row, toRight(col)));
+            if(openSite[matrixToArray(row, toRight(col))] == 1) {
+                uf.union(current, matrixToArray(row, toRight(col)));
             }
-            if(openSite[matrixToArray(row, toLeft(col))] ==1){
-                uf.union(current,matrixToArray(row, toLeft(col)));
+            if(openSite[matrixToArray(row, toLeft(col))] == 1) {
+                uf.union(current, matrixToArray(row, toLeft(col)));
             }
         }
     }
 
-    private boolean isLeft(int col){
+    private boolean isLeft(int col) {
         return col == 0;
     }
 
-    private boolean isRight(int col){
-        return col == (sideLength -1);
+    private boolean isRight(int col) {
+        return col == (sideLength - 1);
     }
 
-    private boolean isTop(int row){
+    private boolean isTop(int row) {
         return row == 0;
     }
 
-    private boolean isBottom(int row){
-        return row == (sideLength -1);
+    private boolean isBottom(int row) {
+        return row == (sideLength - 1);
     }
 
-    private int toRight(int col){
-        return col+1;
+    private int toRight(int col) {
+        return col + 1;
     }
 
-    private int toLeft(int col){
-        return col-1;
+    private int toLeft(int col) {
+        return col - 1;
     }
 
-    private int toBottom(int row){
-        return row+1;
+    private int toBottom(int row) {
+        return row + 1;
     }
 
-    private int toTop(int row){
-        return row-1;
+    private int toTop(int row) {
+        return row - 1;
     }
 
 
@@ -214,10 +213,12 @@ public class Percolation {
             int row = scanner.nextInt();
             int col = scanner.nextInt();
             percolation.open(row, col);
+            percolation.isOpen(row, col);
             if (percolation.percolates()) {
                 System.out.println("Is Percolating");
                 return;
             }
+            percolation.isFull(row, col);
         }
         System.out.println("Does not Percolate");
     }
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -3,24 +3,23 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private  Percolation percolation;
-    private int T;
+    private final int T;
     private double stddev;
     private double mean;
-    private double[] openSites;
+    private final double[] openSites;
     public PercolationStats(int n, int trails){
         if(n <= 0 || trails <=0) throw new IllegalArgumentException("either n or trail is <= 0");
         T = trails;
         double N = n*n;
         openSites = new double[T];
         for(int i=0;i<T; i++) {
-            percolation = new Percolation(n);
-            while(!percolation.percolates()){
-                int row = StdRandom.uniform(1,n+1);
-                int col = StdRandom.uniform(1,n+1);
+            Percolation percolation = new Percolation(n);
+            while(!percolation.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
                 percolation.open(row, col);
             }
-            openSites[i] = (double)percolation.numberOfOpenSites()/N;
+            openSites[i] = (double) percolation.numberOfOpenSites() / N;
         }
     }
     public double mean(){
@@ -42,8 +41,8 @@ public class PercolationStats {
     }
 
     public static void main(String[] args){
-        int n = Integer.valueOf(args[0]);
-        int T = Integer.valueOf(args[1]);
+        int n = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
         PercolationStats stats = new PercolationStats(n, T);
         StdOut.println("mean                    = " + stats.mean());
         StdOut.println("stddev                  = " + stats.stddev());
